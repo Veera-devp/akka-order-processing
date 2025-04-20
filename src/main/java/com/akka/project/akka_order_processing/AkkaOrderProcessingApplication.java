200c6903e48eaea4a0b55f6d3a716f90c29167ae
Veera-devp <saikam.raghava@gmail.com>
2025-04-20 10:46:51 +0530

    first commit

---
+++ src/main/java/com/akka/project/akka_order_processing/AkkaOrderProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.akka.project.akka_order_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaOrderProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkaOrderProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akka/project/akka_order_processing/AkkaOrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akka.project.akka_order_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkaOrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
